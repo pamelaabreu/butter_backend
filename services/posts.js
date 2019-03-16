2b3edbe439305811a9b32ca2da3948d57ed1a31a
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-16 19:29:43 -0400

    Added PostService.create function in post service file

--- services/posts.js
+++ services/posts.js
@@ -1,4 +1,14 @@
 const {db} = require('./dbConnect');
 const PostService = {};
 
+PostService.create = (user_posted_id, tag_id, content_url, title, summary, caption, likes) => {
+    const sql = `
+    INSERT INTO posts (user_posted_id, tag_id, content_url, title, summary, caption, likes) VALUES
+    ($[user_posted_id], $[tag_id], $[content_url], $[title], $[summary], $[caption], $[likes]);`;
+
+    return db.one(sql, { user_posted_id, tag_id, content_url, title, summary, caption, likes })
+}
+
+
+
 module.exports = PostService;
