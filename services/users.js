38dda64b03b1a6db2ccb017b40c94c9af5667e34
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-16 18:18:40 -0400

    Wrote post/create function for users table

--- services/users.js
+++ services/users.js
@@ -1,4 +1,15 @@
 const {db} = require('./dbConnect');
-const UsersService = {};
+const UserService = {};
 
-module.exports = UsersService;
+// POST - CREATE
+UserService.create = (birthname, username, email, firebase_uid, profile_img, birthday, joining_reason, followers_number, followings_number) => {
+    const sql = `
+    INSERT INTO users (birthname, username, email, firebase_uid, profile_img, birthday, joining_reason, followers_number, followings_number) VALUES
+    ($[birthname], $[username], $[email], $[firebase_uid], $[profile_img], $[birthday], $[joining_reason], $[followers_number], $[followings_number]);`;
+
+    return db.one(sql, {birthname, username, email, firebase_uid, profile_img, birthday, joining_reason, followers_number, followings_number});
+}
+
+
+
+module.exports = UserService;
