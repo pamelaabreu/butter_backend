f51c58fb807024c7b300ba4721b0647f4ecd457b
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-16 21:38:37 -0400

    Created FollowService.create function

--- services/follows.js
+++ services/follows.js
@@ -1,4 +1,12 @@
 const {db} = './dbConnect';
 const FollowService = {};
 
+FollowService.create = (user_follower_id, user_following_id) => {
+    const sql = `
+    INSERT INTO follows (user_follower_id, user_following_id) VALUES
+    ($[user_follower_id], $[user_following_id]);`;
+
+    return db.one(sql, { user_follower_id, user_following_id });
+};
+
 module.exports = FollowService;
