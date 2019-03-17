a87a187c74b0afd93d549aa9be8b17a1b670cafb
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-17 12:08:39 -0400

    Created SavedPostService.create function

--- services/saved_posts.js
+++ services/saved_posts.js
@@ -1,4 +1,13 @@
 const {db} = require('./dbConnect');
 const SavedPostService = {};
 
+SavedPostService.create = (user_saved_id, post_saved_id) => {
+    const sql = `
+    INSERT INTO saved_posts (user_saved_id, post_saved_id) VALUES
+    ($[user_saved_id], $[post_saved_id]);`;
+
+    return db.one(sql, { user_saved_id, post_saved_id });
+};
+
+
 module.exports = SavedPostService;
