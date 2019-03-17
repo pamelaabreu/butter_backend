dd9165f1c7ab15221339905190b31c3654d67de7
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-16 20:59:03 -0400

    Created CommentsService.read function

--- services/comments.js
+++ services/comments.js
@@ -9,5 +9,16 @@ CommentsService.create = (user_commented_id, post_commented_id, comment) => {
     return db.one(sql, { user_commented_id, post_commented_id, comment });
 }
 
+CommentsService.read = (id) => {
+    const sql = `
+    SELECT
+        comments.*
+    FROM comments
+    WHERE
+        comments.id = $[id]
+    `;
+
+    return db.one(sql, { id });
+};
 
 module.exports = CommentsService;
