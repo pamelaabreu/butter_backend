1a3f76873690dd1ed49d54777a06122cfee8d35f
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-17 12:11:14 -0400

    Created SavedPostService.update function

--- services/saved_posts.js
+++ services/saved_posts.js
@@ -21,5 +21,20 @@ SavedPostService.read = (id) => {
     return db.one(sql, { id });
 };
 
+SavedPostService.update = (id, user_saved_id, post_saved_id) => {
+    const updated_at = Date.now();
+    const sql = `
+    UPDATE saved_posts
+    SET
+        updated_at = $[updated_at],
+        user_saved_id = $[user_saved_id],
+        post_saved_id = $[post_saved_id]
+    WHERE
+        id=$[id]
+    `;
+
+    return db.none(sql, { id, updated_at, user_saved_id, post_saved_id });
+};
+
 
 module.exports = SavedPostService;
