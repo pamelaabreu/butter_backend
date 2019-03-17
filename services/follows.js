55608a48f4f14330681238e5d761d967a6520cac
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-16 21:39:37 -0400

    Created FollowService.read function

--- services/follows.js
+++ services/follows.js
@@ -9,4 +9,16 @@ FollowService.create = (user_follower_id, user_following_id) => {
     return db.one(sql, { user_follower_id, user_following_id });
 };
 
+FollowService.read = (id) => {
+    const sql = `
+    SELECT
+        follows.*
+    FROM follows
+    WHERE
+        follows.id = $[id]
+    `;
+
+    return db.one(sql, { id });
+};
+
 module.exports = FollowService;
