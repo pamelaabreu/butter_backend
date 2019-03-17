da57c2540ceae24e43e97a1108a22ffa13c4c663
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-17 11:47:35 -0400

    Created LikeService.create function

--- services/likes.js
+++ services/likes.js
@@ -1,4 +1,13 @@
 const {db} = require('./dbConnect');
 const LikeService = {};
 
+LikeService.create = (user_like_id, post_like_id) => {
+    const sql = `
+    INSERT INTO likes (user_like_id, post_like_id) VALUES
+    ($[user_like_id], $[post_like_id]);`;
+
+    return db.one(sql, { user_like_id, post_like_id });
+};
+
+
 module.exports = LikeService;
