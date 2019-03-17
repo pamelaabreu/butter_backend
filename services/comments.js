dbc3165b6dd940ba616534e1cb0b7870a2cfd72e
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-16 20:58:09 -0400

    Created CommentsService.create function

--- services/comments.js
+++ services/comments.js
@@ -1,4 +1,13 @@
 const {db} = require('./dbConnect');
 const CommentsService = {};
 
+CommentsService.create = (user_commented_id, post_commented_id, comment) => {
+    const sql = `
+    INSERT INTO comments (user_commented_id, post_commented_id, comment) VALUES
+    ($[user_commented_id], $[post_commented_id],$[comment]);`;
+
+    return db.one(sql, { user_commented_id, post_commented_id, comment });
+}
+
+
 module.exports = CommentsService;
