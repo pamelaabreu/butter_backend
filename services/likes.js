034ce3a6a1d9d12c96aee8f8180e8407b6927251
Pamela Abreu <pamelaabreu@pursuit.com>
2019-03-17 11:49:11 -0400

    Created LikeService.read function

--- services/likes.js
+++ services/likes.js
@@ -9,5 +9,16 @@ LikeService.create = (user_like_id, post_like_id) => {
     return db.one(sql, { user_like_id, post_like_id });
 };
 
+LikeService.read = (id) => {
+    const sql = `
+    SELECT
+        likes.*
+    FROM likes
+    WHERE
+        likes.id = $[id]
+    `;
+
+    return db.one(sql, { id });
+};
 
 module.exports = LikeService;
